4072a35563219eac8dfb04be462ffa0e597e89d6
Rodrigo Dias Moreira <31393852+rodrigodmoreira@users.noreply.github.com>
2025-06-18 10:09:14 -0300

    Update gitlab-runner.sh

--- gitlab-runner.sh
+++ gitlab-runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # sudo
+# https://docs.gitlab.com/runner/install/linux-repository/
 curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | bash
 apt install gitlab-runner
